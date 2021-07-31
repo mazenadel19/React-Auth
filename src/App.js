57e3caa3e130d4a5810b53bf5a1d9b613c9b56e1
mazen <mazenadel19@gmail.com>
2021-07-31 15:15:00 +0200

    navigation guards 👏

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import { Switch, Route } from 'react-router-dom';
-
-import Layout from './components/Layout/Layout';
-import UserProfile from './components/Profile/UserProfile';
-import AuthPage from './pages/AuthPage';
-import HomePage from './pages/HomePage';
+import { useContext } from 'react'
+import { Redirect, Route, Switch } from 'react-router-dom'
+import Layout from './components/Layout/Layout'
+import UserProfile from './components/Profile/UserProfile'
+import AuthPage from './pages/AuthPage'
+import HomePage from './pages/HomePage'
+import AuthContext from './store/auth-context'
 
 function App() {
-  return (
-    <Layout>
-      <Switch>
-        <Route path='/' exact>
-          <HomePage />
-        </Route>
-        <Route path='/auth'>
-          <AuthPage />
-        </Route>
-        <Route path='/profile'>
-          <UserProfile />
-        </Route>
-      </Switch>
-    </Layout>
-  );
+	const authContext = useContext(AuthContext)
+
+	const { isLoggedIn } = authContext
+
+	return (
+		<Layout>
+			<Switch>
+				<Route path='/' exact>
+					<HomePage />
+				</Route>
+
+				{!isLoggedIn && (
+					<Route path='/auth'>
+						<AuthPage />
+					</Route>
+				)}
+
+				<Route path='/profile'>
+					{isLoggedIn && <UserProfile />}
+					{!isLoggedIn && <Redirect to='/' />}
+				</Route>
+
+				<Route path='*'>
+					<Redirect to='/' />
+				</Route>
+			</Switch>
+		</Layout>
+	)
 }
 
-export default App;
+export default App
